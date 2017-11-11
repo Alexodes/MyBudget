8bf0bfbad7e88b96a1e571372092e18f4c9cf2ca
alexodes <bi2rbo@mail.ru>
2017-11-11 14:02:11 +0200

    Login_Logout

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { startLogin } from '../actions/auth';
 
-export const Login = () => (
-  <div>
-      <button>Login</button>
-  </div>
+export const Login = ({ startLogin }) => (
+    <div>
+        <button onClick={startLogin}>Login</button>
+    </div>
 );
 
+const mapDispatchToProps = (dispatch) => ({
+    startLogin: () => dispatch(startLogin())
+});
+
+export default connect(undefined, mapDispatchToProps)(Login);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import AppRouter from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { startSetExpenses } from "./actions/expenses";
-import { setTextFilter } from "./actions/filters";
+import { startSetExpenses } from './actions/expenses';
+import { login, logout } from './actions/auth';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './style.css';
+import 'react-dates/lib/css/_datepicker.css';
 import { firebase } from './firebase/firebase';
 
 
@@ -20,17 +21,28 @@ const jsx = (
     </Provider>
 );
 
+let hasRendered = false;
+const renderApp = () => {
+    if (!hasRendered) {
+        ReactDOM.render(jsx , document.getElementById('root'));
+        hasRendered = true;
+    }
+};
+
 ReactDOM.render(<p>Loading...</p>, document.getElementById('root'));
-
-store.dispatch(startSetExpenses()).then(() => {
-    ReactDOM.render(jsx , document.getElementById('root'));
-});
-
 
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-
+        store.dispatch(login(user.uid));
+        store.dispatch(startSetExpenses()).then(() => {
+            renderApp();
+            if (history.location.pathname === '/') {
+                history.push('/dashboard');
+            }
+        });
     } else {
-
+        store.dispatch(logout());
+        renderApp();
+        history.push('/');
     }
 });
